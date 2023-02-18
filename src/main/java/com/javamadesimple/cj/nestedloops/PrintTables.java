983ffa50da88b5f6aac0271b627806f8b44b2672
manya <manyament100@gmail.com>
2023-02-18 09:00:23 -0800

    added method to find the biggest of three numbers

--- src/main/java/com/javamadesimple/cj/nestedloops/PrintTables.java
+++ src/main/java/com/javamadesimple/cj/nestedloops/PrintTables.java
@@ -2,12 +2,27 @@ package com.javamadesimple.cj.nestedloops;
 
 public class PrintTables {
     public static void main(String[] args) {
-        for(int i = 1; i <= 20;i++) {
+        for(int i = 1; i <= 100;i++) {
             int num = i;
             for(int k = 1; k <= 10; k++){
                 System.out.println(num + " * " + k + " = " + num * k);
             }
             System.out.println("-".repeat(20));
         }
+        System.out.println(biggest(20, 30, 40));
     }
+
+
+    public static int biggest(int a, int b, int c) {
+        int big;
+        if (a > b && a > c) {
+            big = a;
+        } else if (b > c) {
+            big = b;
+        } else {
+            big = c;
+        }
+        return big;
+    }
+
 }
